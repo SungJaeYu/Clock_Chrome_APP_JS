2039e309851afa751affd613e11edc6f9b4406f8
SungJaeYu <ysj7045@naver.com>
2020-10-21 23:35:08 +0900

    Third : Made ToDo Print

--- todo.js
+++ todo.js
@@ -1,34 +1,52 @@
-const form = document.querySelector(".js-toDoForm"),
-    toDoInput = form.querySelector("input"),
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = 'toDos';
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+	localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos != null){
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+		const parsedToDos = JSON.parse(loadedToDos);
+		console.log(parsedToDos);
+		parsedToDos.forEach(function(toDo){
+			paintToDo(toDo.text);
+		});
     }
 }
 function paintToDo(text){
     const li = document.createElement("li");
-    const delBtn = doucument.createElement("button");
-    delBtn.value = "X";
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
     const span = document.createElement("span");
-    span.innerText = text;
+    const newId = toDos.length + 1;
+	span.innerText = text;
     li.appendChild(span);
     li.appendChild(delBtn);
+	li.id = newId;
     toDoList.appendChild(li);
+	const toDoObj = {
+		text: text,
+		id: newId
+	};
+	toDos.push(toDoObj);
+	saveToDos();
 }
-function handleSubmit(EVENT){
-    EVENT.PREVENTdEFAULT();
-    const currentValue = toDoInput.value;
+function handleSubmit(event){
+    event.preventDefault();
+	const currentValue = toDoInput.value;
     paintToDo(currentValue);
     toDoInput.value = "";
 }
 function init(){
     loadToDos();
-    toDoForm.addEventListener("submit", handelSubmit);
+    toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
